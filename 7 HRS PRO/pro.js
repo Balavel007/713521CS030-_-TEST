c1ff5f88637af5c22d3df8b03db3736e2f70701a
Balavel007 <palanivelbala143@gmail.com>
2024-07-18 16:28:10 +0530

    Add files via upload

---
+++ 7 HRS PRO/pro.js
@@ -0,0 +1,10 @@
+function log() {
+    window.alert("LOGIN SUCCESSFULLY COMPLEATED");
+    
+}
+function sign(){
+    window.alert("sign up successfull");
+}
+function logout(){
+    window.alert(" THANK YOU FOR COMING ! ");
+}
